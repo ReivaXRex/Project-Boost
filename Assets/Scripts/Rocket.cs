067c21cfa1f23f73ac8a79ae1d611050433babef
Xavier Faria <xavierfaria@outlook.com>
2021-01-27 21:36:16 -0500

    Lesson 48 Finished. Audio implemented.

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rb;
+   // bool isFlying;
+    AudioSource thrustAudio;
+
+    [SerializeField] int thrustPower = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        thrustAudio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up);
+            if (!thrustAudio.isPlaying)
+            {
+                thrustAudio.Play();
+            }
+        }
+        else
+        {
+            thrustAudio.Stop();
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * thrustPower * Time.deltaTime);
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * thrustPower * Time.deltaTime);
+        }    
+    }
+}
