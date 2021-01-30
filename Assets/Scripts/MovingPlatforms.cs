77fa82aad91a580a06423384214988a9015c7ec8
Xavier Faria <xavierfaria@outlook.com>
2021-01-30 14:27:53 -0500

    Section Finished. 3 Levels completed. SFX added.

---
+++ Assets/Scripts/MovingPlatforms.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class MovingPlatforms : MonoBehaviour
+{
+    [SerializeField] Vector3 movementVector;
+    [SerializeField] float period = 2f; // Time it takes to complete one cycle.
+   
+    [Range(0, 1)] [SerializeField] float movementFactor;
+    Vector3 startingPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPos = transform.position; // Set the startingPos to the initial position of the Transform.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (period <= Mathf.Epsilon) { return; } // protect against period being 0.
+      
+        float cycles = Time.time / period; // grows continually from 0
+
+        const float tau = Mathf.PI * 2; // Tau is 2 Pi, about 6.28
+        float rawSinWave = Mathf.Sin(cycles * tau);
+
+        movementFactor = rawSinWave / 2f + 0.5f;
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPos + offset;
+    }
+}
--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -1,83 +1,164 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
-    Rigidbody rb;
-    AudioSource thrustAudio;
+    private Rigidbody rb;
+    private AudioSource audioSource;
 
     [SerializeField] int thrustPower;
     [SerializeField] int rotateThrustPower;
+    [SerializeField] float levelLoadDelay = 1f;
+    
+    [SerializeField] AudioClip rocketEngine;
+    [SerializeField] AudioClip deathAudio;
 
+    [SerializeField] ParticleSystem engineSFX;
+    [SerializeField] ParticleSystem deathSFX;
+    [SerializeField] ParticleSystem winSFX;
 
-    void Start()
+    private enum State
+    { Alive, Dying, Transcending }
+
+    [SerializeField] private State state = State.Alive;
+
+    private void Start()
     {
         rb = GetComponent<Rigidbody>();
-        thrustAudio = GetComponent<AudioSource>();
+        audioSource = GetComponent<AudioSource>();
     }
 
-    void Update()
+    private void Update()
     {
-        Thrust();
-        Rotate();
+        if (state != State.Dying)
+        {
+            RespondToInput();
+            Rotate();
+        }
     }
 
-    private void Thrust()
+    private void RespondToInput()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            rb.AddRelativeForce(Vector3.up * thrustPower);
-            
-            if (!thrustAudio.isPlaying) // Prevent clip from layering.
-            {
-                thrustAudio.Play();
-            }
+            ApplyForce();
         }
-
         else
         {
-            thrustAudio.Stop();
-        }      
+            StopThrustEffects();
+        }
+    }
+
+    private void StopThrustEffects()
+    {
+        audioSource.Stop();
+        engineSFX.Stop();
+    }
+
+    private void ApplyForce()
+    {
+        rb.AddRelativeForce(Vector3.up * thrustPower * Time.deltaTime);
+
+        if (!audioSource.isPlaying) // Prevent clip from layering.
+        {
+            audioSource.PlayOneShot(rocketEngine);
+        }
+
+        engineSFX.Play();
     }
 
     private void Rotate()
     {
-        rb.freezeRotation = true; // take manual control of rotation.
-       
+        rb.angularVelocity = Vector3.zero; // remove rotation due to Physics.
+
         float rotation = rotateThrustPower * Time.deltaTime;
-        
+
         if (Input.GetKey(KeyCode.A))
         {
-         
             transform.Rotate(Vector3.forward * rotation);
         }
-
         else if (Input.GetKey(KeyCode.D))
         {
             transform.Rotate(-Vector3.forward * rotation);
         }
 
-        rb.freezeRotation = false; // resume physics control of rotation.
-
+       
     }
 
     private void OnCollisionEnter(Collision collision)
     {
+        if (state != State.Alive)
+        {
+            return;
+        }
         switch (collision.gameObject.tag)
         {
             case "Friendly":
-                Debug.Log(collision.gameObject.name);
+              //  Debug.Log(collision.gameObject.name);
                 break;
+
             case "Obstacles":
-                Debug.Log(collision.gameObject.name + " You died");
+              //  Debug.Log(collision.gameObject.name + " You died");
+                StartDeathSequence();
                 break;
+
             case "Finish":
-                Debug.Log(collision.gameObject.name + " You won!");
+               // Debug.Log(collision.gameObject.name + " You won!");
+                StartLoadSequence();
                 break;
 
             default:
                 break;
+        }
+    }
+
+    private void StartDeathSequence()
+    {
+        state = State.Dying;
+        audioSource.Stop();
+        audioSource.PlayOneShot(deathAudio);
+        deathSFX.Play();
+        Invoke("LoadNextLevel", levelLoadDelay);
+    }
+
+    private void StartLoadSequence()
+    {
+        state = State.Transcending;
+        //   audioSource.Stop();
+        //   audioSource.PlayOneShot(loadAudio);
+        winSFX.Play();
+        Invoke("LoadNextLevel", levelLoadDelay);
+    }
+
+    private void LoadNextLevel()
+    {
+        if (state == State.Dying)
+        {
+            SceneManager.LoadScene(0);
+            state = State.Alive;
+        }
+        else if (state == State.Transcending)
+        {
+            int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+            int nextSceneIndex = 0;
+
+            switch (currentSceneIndex)
+            {
+                case 0:
+                    nextSceneIndex = 1;
+                    break;
+                case 1:
+                    nextSceneIndex = 2;
+                    break;
+                case 2:
+                    nextSceneIndex = 0;
+                    break;
+                default:
+                    break;
+            }
+          
+            SceneManager.LoadScene(nextSceneIndex);
+            state = State.Alive;
         }
     }
 }
